a167059f6526eb6f51af7a3b0dbba0412008cec2
mamizerov <arsenal74@mail.ru>
2020-01-16 20:49:44 +0300

    Add project files.

---
+++ WebStore/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Models;
+
+namespace WebStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly List<EmployeeView> _employees = new List<EmployeeView>
+        {
+            new EmployeeView
+            {
+                Id = 1,
+                FirstName = "Иван",
+                SurName = "Иванов",
+                Patronymic = "Иванович",
+                Age = 22
+            },
+            new EmployeeView
+            {
+                Id = 2,
+                FirstName = "Владислав",
+                SurName = "Петров",
+                Patronymic = "Иванович",
+                Age = 35
+            }
+        };
+
+        // GET: /
+        // GET: /home/
+        // GET: /home/index
+        public IActionResult Index()
+        {
+            return View(_employees);
+            //return Content("Hello from controller");
+        }
+
+        // GET: /home/details/{id}
+        public IActionResult Details(int id)
+        {
+            var employee = _employees.FirstOrDefault(x => x.Id == id);
+
+            //Если такого не существует
+            if (employee == null)
+                return NotFound(); // возвращаем результат 404 Not Found
+
+            return View(employee);
+        }
+
+    }
+}
---
+++ WebStore/Startup.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace WebStore
+{
+    public class Startup
+    {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //Добавляем сервисы, необходимые для mvc
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            var helloMessage = _configuration["CustomHelloWorld"];
+            var logLevel = _configuration["Logging:LogLevel:Microsoft"];
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                // Маршрут по умолчанию состоит из трёх частей разделённых “/”
+                // Первой частью указывается имя контроллера,
+                // второй - имя действия (метода) в контроллере,
+                // третей - опциональный параметр с именем “id”
+                // Если часть не указана - используются значения по умолчанию:
+                // для контроллера имя “Home”,
+                // для действия - “Index”
+            });
+        }
+    }
+}
